39fd02c86e33bd025fce692e724ff4816ce3657f
alive1258 <javashazib@gmail.com>
2023-06-18 14:32:33 +0600

    project done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>header</h1>
+        </div>
+    );
+};
+
+export default Header;
